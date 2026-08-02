074f9a8c757a3b612ade7ddc60adfb628539c428
AbhinavRdy <user.abreddy@gmail.com>
2026-08-02 11:34:19 +0530

    Add Firestore integration with full CRUD operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+//jsx
+import { useState, useEffect } from "react"
+import { db } from "./firebase"
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [tasks, setTasks] = useState([])
+  const [newTaskText, setNewTaskText] = useState("")
+  const [editingID, setEditingID] = useState(null)
+  const [editText, setEditText] = useState("")
+
+  useEffect(() => { 
+    async function fetchTasks() {
+      const querySnapshot = await getDocs(collection(db, "tasks")) // await - pause this function until data comes.
+      const loadedTasks = querySnapshot.docs.map((docSnap) => ({
+        id: docSnap.id,
+        ...docSnap.data(),
+      }))
+      setTasks(loadedTasks)      
+    }
+    fetchTasks()
+  }, []) // [] means , run only once, not on every re-render.
+
+  async function addTask(e) { // aysnc - wait for slow operations.
+    e.preventDefault() //prevent to reload page
+    if (newTaskText.trim() === "") return // trim removes leading/trailing whitespace.
+
+    const docRef = await addDoc(collection(db, "tasks"),{ // addDoc creates a new doc.
+       text: newTaskText,
+       done: false,
+    })
+    setTasks([...tasks, {id: docRef.id, text: newTaskText, done: false}])
+    setNewTaskText("")
+  }
+
+  function startEditing(task){
+    setEditingID(task.id)
+    setEditText(task.text)
+  }
+
+  async function saveEdit(id) {
+    if (editText.trim() === "") return
+    await updateDoc(doc(db, "tasks", id), {text: editText})
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {...task, text: editText } : task
+      )
+    )
+    setEditingID(null)
+  }
+
+  async function toggleDone(id, currentDone) {
+    await updateDoc(doc(db, "tasks", id), { done: !currentDone})
+    setTasks(
+      tasks.map((task) =>
+      task.id === id ? {...task, done: !currentDone } :task // a ternary operator
+      )
+    )
+  }
+
+  async function deleteTask(id) {
+    await deleteDoc(doc(db, "tasks", id))
+    setTasks(tasks.filter((task) => task.id !== id)) // filter() -> array method, returns a new array, never mutates.
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>Journal Flow</h1>
 
-      <div className="ticks"></div>
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          value={newTaskText}
+          onChange={(e) => setNewTaskText(e.target.value)} // e is the event object
+          placeholder="Add a task..."
+        />
+          <button type="submit">Add</button>
+      </form>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+             <input
+               type="checkbox"
+               checked={task.done}
+               onChange={() => toggleDone(task.id, task.done)}
+             />
+             {editingID === task.id ? ( // conditional rendering
+              <>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button onClick={() => saveEdit(task.id)}>Save</button>
+                </>
+             ) : (
+              <>
+                {task.text}
+                <button onClick={() => startEditing(task)}>Edit</button>
+              </>
+             )}
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
